3ced9d2ad83f2a55783b98968b6ae9c7cd669c65
Chapin Bryce <chapin@chapinbryce.com>
2017-10-01 13:02:40 -0400

    Update to indexing process and start of query development

--- aws_ip_tracker.py
+++ aws_ip_tracker.py
@@ -2,9 +2,9 @@ import argparse
 import datetime
 import json
 import os
-from pdb import set_trace as st
 
 from netaddr import *
+from pymongo import MongoClient
 from tqdm import tqdm
 
 """
@@ -38,12 +38,45 @@ __description__ = "Utility to read AWS IP Address mappings into MongoDB"
 
 class ParseIPs(object):
     def __init__(self, **kwargs):
-        self.tqdm = False
-        if "tqdm" in kwargs:
-            self.tqdm = True
+        self.tqdm = kwargs.get('tqdm', False)
+        self.host = kwargs.get('host', 'localhost')
+        self.port = kwargs.get('port', 27017)
 
+    def parse(self, json_file):
+        # Include DB support here
+        mongo = MongoClient(self.host, self.port)
+        db = mongo.IPTracker
+        posts = db.aws_ip_ranges
 
-    def parse_json_file(self, json_file):
+        records = self.parse_ips._parse_json_file(json_file)
+        if self.tqdm:
+            looper = tqdm(records))
+        else:
+            looper = records
+        for rec in looper:
+            matching_posts = posts.find({'first_ip': rec['first_ip'],
+                                         'last_ip': rec['last_ip'],
+                                         'cidr': rec['cidr']})
+
+            if matching_posts.count() == 1:
+                # Update prior record
+                matching_rec = matching_posts.next()
+                events = rec.get("events", []) + matching_rec.get("events", [])
+                posts.update_one({'first_ip': rec['first_ip'],
+                                 'last_ip': rec['last_ip'],
+                                 'cidr': rec['cidr']},
+                                {"$set": {"events": events}})
+            elif matching_posts.count() > 1:
+                # Throw error, should not have more than 1...
+                raise Exception("Too many posts matching {} found".format(
+                    str(rec)
+                ))
+            else:
+                # Insert new record
+                posts.insert_one(rec)
+        print("All items loaded")
+
+    def _parse_json_file(self, json_file):
         """Parse json file into individual records for the database
 
         Args:
@@ -52,9 +85,8 @@ class ParseIPs(object):
         """
         json_data = json.load(json_file)
         collected_date = datetime.datetime.strptime(
-            os.path.basename(json_file.name).split(
-                "_")[1].replace(".json", ""),
-            "%Y%m%d%H%M%S"
+            os.path.basename(json_file.name).split("_")[1].replace(
+                ".json", ""), "%Y%m%d%H%M%S"
         )
         record_created = datetime.datetime.strptime(
             json_data.get("createDate"), "%Y-%m-%d-%H-%M-%S"
@@ -64,17 +96,48 @@ class ParseIPs(object):
             looper = tqdm(json_data.get("prefixes", []))
         else:
             looper = json_data.get("prefixes", [])
-        for prefix in looper:
-            records = []
-            prefix["record_created"] = record_created
-            prefix["record_collected"] = collected_date
-            ip_net = IPNetwork(prefix.get("ip_prefix", ""))
-            for net in ip_net:
-                rec = prefix.copy()
-                rec['ip_addr'] = str(net)
-                records.append(rec)
 
-            yield records
+        # Sample Record
+        {
+            # Lookup attributes
+            "first_ip": "10.10.10.0",
+            "last_ip": "10.10.10.255",
+            "cidr": "10.10.10.0/16",
+            "events": [
+                # Recorded events
+                {"record_created": "", "record_collected": "",
+                 "region": "", "service": ""}
+            ]
+        }
+
+        records = []
+        for prefix in looper:
+            event = {
+                "record_created": record_created,
+                "record_collected": collected_date,
+                "region": prefix.get("region", ""),
+                "service": prefix.get("service", "")
+            }
+            ip_net = IPNetwork(prefix.get("ip_prefix", ""))
+            records.append({
+                "first_ip": ip_net.first,
+                "last_ip": ip_net.last,
+                "cidr": prefix.get("ip_prefix", ""),
+                "events": [
+                    event
+                ]
+            })
+
+        return records
+
+
+class QueryIP(object):
+    """
+    To Query IP:
+        convert ip into <ip_int> with int(netaddr.IPAddress())
+        {'first_ip': { $lte: <ip_int> } , 'last_ip': { $gte: <ip_int> } }
+    """
+    pass
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
@@ -86,21 +149,10 @@ if __name__ == '__main__':
     )
     parser.add_argument("JSON_FILE", help='Path to JSON file to parse',
                         type=argparse.FileType('r'))
-
+    parser.add_argument("--mongo-host", help="Host of MongoDB instance",
+                        default='localhost')
+    parser.add_argument("--mongo-port", help="Host of MongoDB instance",
+                        default=27017, type=int)
     args = parser.parse_args()
 
-    # Include DB support here
-    from pymongo import MongoClient
-    mongo = MongoClient('localhost', 27017)
-    db = mongo.IPTracker
-    posts = db.aws_ips
-
-    chunk_size = 5000
-    pIP = ParseIPs(tqdm=True)
-    for rset in pIP.parse_json_file(args.JSON_FILE):
-        for x in tqdm(range(0, len(rset), chunk_size), leave=False):
-            recs = posts.insert_many(rset[x:x+chunk_size], ordered=False)
-            if len(recs.inserted_ids) != len(rset[x:x+chunk_size]):
-                print("{} records of {} results loaded into the db".format(
-                    len(recs.inserted_ids), len(rset[x:x+chunk_size])))
-    print("All items loaded")
+    parse_ips = ParseIPs(tqdm=True, host=args.mongo_host, port=args.mongo_port)
